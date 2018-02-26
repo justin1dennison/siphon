fc5f41caff0ea45ed91fb90ba876c26341857daf
Travis CI <travis@nobody.org>
2018-02-26 16:59:34 +0000

    Deploy dev to GitHub Pages

---
+++ dev/_static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '0.6.1+34.g2a9cc97',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
